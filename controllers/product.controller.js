af34f6b2591bdef90f15a31bf0bed4b4f5d94be2
Jihee <sjh709@naver.com>
2024-06-18 20:44:51 +0900

    페이지네이션

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require('../models/Product');
 
+const PAGE_SIZE = 3;
+
 const productController = {};
 
 productController.createProduct = async (req, res) => {
@@ -38,9 +40,17 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: 'i' } } : {};
     let query = Product.find(cond).select('-createdAt -__v');
+    let response = { status: 'success' };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.find(cond).count();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     const productList = await query.exec();
-    res.status(200).json({ status: 'success', data: productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
