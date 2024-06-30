63e71ca252a01f011facf32149b9fb7cd12c6e85
Jihee <sjh709@naver.com>
2024-06-30 16:14:23 +0900

    구글 로그인

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
 
 const authController = {};
 
@@ -21,6 +23,34 @@ authController.loginWithEmail = async (req, res) => {
     throw new Error('이메일이 존재하지 않습니다.');
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+authController.loginWithGoogle = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    let user = await User.findOne({ email });
+    if (!user) {
+      const randomPassword = '' + Math.floor(Math.random() * 100000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+      });
+      await user.save();
+    }
+    const sessionToken = await user.generateToken();
+    res.status(200).json({ status: 'success', user, token: sessionToken });
+  } catch (error) {
+    return res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
--- routes/auth.api.js
+++ routes/auth.api.js
@@ -5,4 +5,7 @@ const router = express.Router();
 // 로그인
 router.post('/login', authController.loginWithEmail);
 
+// 구글로그인
+router.post('/google', authController.loginWithGoogle);
+
 module.exports = router;
